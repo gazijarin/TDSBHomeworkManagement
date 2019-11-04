2a8a97cba1c4c57a0353073b8fe6174c610ad8ba
Raiyan Rahman <ray.rahmn@hotmail.com>
2019-11-03 20:13:30 -0500

    Made background static.

--- front-end/src/components/home/home.vue
+++ front-end/src/components/home/home.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <p>This is the home page</p>
+  <div class="col-12" id="container" style="margin-top: 5%;">
+    <div class="col-4" style="float: right; padding-top: 15px;">
+      <b-card border-variant="dark">
+        Calendar here.
+      </b-card>
+      <br/>
+      <b-card border-variant="dark">
+        Task list here.
+      </b-card>
+    </div>
+    <div class="col-8" style="padding-top: 15px">
+      <b-card class="today-container"
+        border-variant="dark"
+        header="Today"
+        align="left">
+          <b-card-body style="margin-top: -25px; margin-bottom: -15px;">
+            No tasks today.
+          </b-card-body>
+      </b-card>
+    </div>
+    <div class="col-8" style="padding-top: 2%">
+      <b-card border-variant="dark" header="Feed" align="left" style="height: 2000px;">
+
+      </b-card>
+    </div>
   </div>
 </template>
 
