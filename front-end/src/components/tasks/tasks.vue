a7efd2b6e29c5d3c57f2804709fa3d7bb2728a11
Gazi Jarin <gazijarin@gmail.om>
2019-11-04 21:23:36 -0500

    Added description box, datepicker, timepicker and other stuff

--- front-end/src/components/tasks/tasks.vue
+++ front-end/src/components/tasks/tasks.vue
@@ -29,29 +29,31 @@
             <div class="row">
               <div class="col-md-6">
                 <div class="form-group">
-                  <b-form-input v-model="text" placeholder="Course"></b-form-input>
+                  <datepicker placeholder="Due Date"></datepicker>
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="form-group">
-                  <b-form-input v-model="text" placeholder="Due Date"></b-form-input>
+                  <datepicker placeholder="Time"></datepicker>
                 </div>
               </div>
             </div>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-              </div>
-              <div class="custom-file">
-                <input
-                  type="file"
-                  class="custom-file-input"
-                  id="inputGroupFile01"
-                  aria-describedby="inputGroupFileAddon01"
-                />
-                <label class="custom-file-label" for="inputGroupFile01">Choose an attachment</label>
+            <div class="form-group">
+              <select class="form-control">
+                <option>Course</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <div id="app">
+                <vue-editor v-model="content" :editorToolbar="customToolbar"></vue-editor>
               </div>
             </div>
+            <form>
+              <div class="form-group">
+                <label for="exampleFormControlFile1">Attachment(s)</label>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1" />
+              </div>
+            </form>
           </b-modal>
         </div>
         <full-calendar :config="config" :events="events" style="margin: 30px"></full-calendar>
@@ -74,36 +76,97 @@
 </template>
 
 <script>
+import Datepicker from "vuejs-datepicker";
+import { VueEditor } from "vue2-editor";
 export default {
-  name: 'Tasks',
-    data() {
+  name: "Tasks",
+  components: {
+    Datepicker,
+    VueEditor
+  },
+  data() {
     return {
+      content: "<h4>Description</h4>",
+      customToolbar: [
+        ["bold", "italic", "underline"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        ["image", "code-block"]
+      ],
       events: [
         {
-            title  : 'event1',
-            start  : '2019-11-01',
+          title: "event1",
+          start: "2019-11-01"
         },
         {
-            title  : 'event2',
-            start  : '2019-11-05',
+          title: "event2",
+          start: "2019-11-05"
         },
         {
-            title  : 'event3',
-            start  : '2019-11-09T12:30:00',
-            allDay : false,
-        },
+          title: "event3",
+          start: "2019-11-09T12:30:00",
+          allDay: false
+        }
       ],
-      
-			config: {
-        defaultView: 'month'
-      },
-    }
-  }
-  
-  
-}
 
+      config: {
+        defaultView: "month"
+      }
+    };
+  }
+};
 </script>
 
 <style>
+body {
+  font-family: "Helvetica Neue Light", Helvetica, sans-serif;
+  padding: 1em 2em 2em;
+}
+input,
+select {
+  padding: 0.75em 0.5em;
+  font-size: 100%;
+  border: 1px solid #ccc;
+  width: 100%;
+}
+
+select {
+  height: 2.5em;
+}
+
+.example {
+  background: #f2f2f2;
+  border: 1px solid #ddd;
+  padding: 0em 1em 1em;
+  margin-bottom: 2em;
+}
+
+code,
+pre {
+  margin: 1em 0;
+  padding: 1em;
+  border: 1px solid #bbb;
+  display: block;
+  background: #ddd;
+  border-radius: 3px;
+}
+
+.settings {
+  margin: 2em 0;
+  border-top: 1px solid #bbb;
+  background: #eee;
+}
+
+h5 {
+  font-size: 100%;
+  padding: 0;
+}
+
+.form-group {
+  margin-bottom: 1em;
+}
+
+.form-group label {
+  font-size: 60%;
+  display: block;
+}
 </style>
