a8a6915726d22e005df04d45df3924224679a047
Michael Katilevsky <katilevsky.michael@gmail.com>
2019-11-11 22:41:03 -0500

    Made login page more aesthetic.

--- front-end/src/components/login/login.vue
+++ front-end/src/components/login/login.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="col-12" id="container" style="margin-top: 5%;">
+    <b-navbar fixed="top" toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand>TDSB App</b-navbar-brand>
+    </b-navbar>
+
+    <div class="slanted" style="background-color: #79BC6D; z-index: -5;"></div>
+    <div class="slanted" style="background-color: #FCB711; margin-left: 11%; z-index: -6;"></div>
+    <div class="slanted" style="background-color: #4277A5; margin-left: 25%; z-index: -7;"></div>
+    <div class="slanted" style="background-color: #EC7B1C; margin-left: 30%; z-index: -8;"></div>
 
     <div class="col-4" style="float: right; padding-top: 100px;">
      <div class="content">
-      <div class="" id="inputForm"> 
+      <div class="" id="inputForm">
         <div>
           <h3 class="headline mb-0"> <b>Welcome Student</b></h3>
           <p class="subheading"><b>Login below using your TDSB google Account </b></p>
         </div>
-        
+
         <button type="primary"
             icon="fas fa-edit"
             @click="handleClickSignIn"
@@ -54,14 +62,14 @@ export default {
       this.$gapi
         .signIn()
         .then(user => {
-          //on success 
+          //on success
 
           const payLoad = {
             _id: user.id,
             first_name: user.firstname,
-            last_name: user.lastname,  
+            last_name: user.lastname,
             image: user.image,
-            email: user.email,  
+            email: user.email,
           }
 
           axios.post('http://localhost:5000/api/students', payLoad)
@@ -73,7 +81,7 @@ export default {
           //const token = GoogleUser.getAuthResponse().access_token;
 
           this.$store.dispatch('setUser', payLoad)
-          
+
           //direct to user home
           this.$router.push({
           name: 'Home'
@@ -112,5 +120,13 @@ export default {
 </script>
 
 <style>
-
+.slanted {
+  height: 100%;
+  width: 50%;
+  position: fixed;
+  top: 0;
+  left: -30%;
+  overflow: hidden;
+  transform: skewX(-15deg);
+}
 </style>
