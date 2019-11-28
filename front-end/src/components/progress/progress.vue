602f4a11b8f7c80a5ac43506f4402f5533fb6ece
Gazi Jarin <gazijarin@gmail.com>
2019-11-28 13:24:14 -0500

    Added progress page

---
+++ front-end/src/components/progress/progress.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Progress</h1>
+    <p>This is the progress page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Progress' //this is the name of the component
+}
+</script>
+<style>
+</style>
