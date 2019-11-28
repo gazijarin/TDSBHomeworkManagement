b3e3cc9a58a3ed839172f980cadac2493a38743f
Gazi Jarin <gazijarin@gmail.com>
2019-11-28 13:56:22 -0500

    Added skeleton front-end for progress page

--- front-end/src/components/progress/progress.vue
+++ front-end/src/components/progress/progress.vue
@@ -1,20 +1,43 @@
 <template>
-  <div>
+  <div class="col-12" id="container" style="margin-top: 5%;">
     <navbar></navbar>
-    <h1>Progress</h1>
-    <p>This is the progress page</p>
+    <div class="col-4" style="float: right; padding-top: 15px;">
+      <div class="card" style="border:1px solid black;">
+        <div class="card-header">
+          <h6 style="float: left; padding-top: 10px">Assignments</h6>
+          <b-button v-b-modal.modal-1 size="sm" style="float: right; margin-left: 15px;">
+            Add Assignment
+            <font-awesome-icon :icon="['fas', 'plus']" />
+          </b-button>
+        </div>
+      </div>
+    </div>
+    <div class="col-8" style="padding-top: 15px">
+      <div class="card" style="border:1px solid black;">
+        <div class="card-header">
+          <h6 style="float: left; padding-top: 10px">Progress</h6>
+        </div>
+      </div>
+    </div>
+    <div class="col-8" style="padding-top: 15px">
+      <div class="card" style="border:1px solid black;">
+        <div class="card-header">
+          <h6 style="float: left; padding-top: 10px">Grades</h6>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import navbar from '../navbar/navbar';
+import navbar from "../navbar/navbar";
 
 export default {
-  name: 'Progress', //this is the name of the component
+  name: "Progress", //this is the name of the component
   components: {
-    'navbar': navbar
+    navbar: navbar
   }
-}
+};
 </script>
 <style>
 </style>
