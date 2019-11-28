9b351637077747cab95641d014064b5b248d6dc0
Gazi Jarin <gazijarin@gmail.com>
2019-11-28 14:57:31 -0500

    Added all the modality for the addAssignment button

--- front-end/src/components/progress/progress.vue
+++ front-end/src/components/progress/progress.vue
@@ -9,6 +9,52 @@
             Add Assignment
             <font-awesome-icon :icon="['fas', 'plus']" />
           </b-button>
+          <b-modal
+            id="modal-1"
+            size="lg"
+            centered
+            ref="modal"
+            title="Add Assignment"
+            @show="resetModal"
+            @hidden="resetModal"
+            @ok="handleOk"
+          >
+            <div class="form-group">
+              <b-form-input v-model="text" placeholder="Title"></b-form-input>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <datepicker placeholder="Due Date"></datepicker>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <input class="form-control" type="time" value="12:00:00" style="margin-top: 2%" />
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <select class="form-control">
+                <option>Course</option>
+              </select>
+            </div>
+            <!-- <div class="form-group">
+              <input type="number" class="form-control" id="GradeInput" placeholder="Grade" />
+            </div> -->
+            <label for="completedAssignment">Amount Completed</label>
+            <div class="form-group">
+              <vue-slider v-model="value" :marks="marks">
+                <template v-slot:step="{ label, active }">
+                  <div :class="['custom-step', { active }]"></div>
+                </template>
+              </vue-slider>
+            </div>
+            <label for="attachments">Attachment(s)</label>
+            <div class="form-group">
+              <input type="file" class="form-control-file" id="exampleFormControlFile1" />
+            </div>
+          </b-modal>
         </div>
       </div>
     </div>
@@ -31,13 +77,35 @@
 
 <script>
 import navbar from "../navbar/navbar";
+import Datepicker from "vuejs-datepicker";
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
 
 export default {
   name: "Progress", //this is the name of the component
   components: {
+    Datepicker,
+    VueSlider,
     navbar: navbar
+  },
+  data() {
+    return {
+      value: 50,
+      marks: val => val % 20 === 0
+    };
   }
 };
 </script>
 <style>
+.custom-step {
+  width: 100%;
+  height: 90%;
+  border-radius: 50%;
+  box-shadow: 0 0 0 3px #ccc;
+  background-color: #fff;
+}
+.custom-step.active {
+  box-shadow: 0 0 0 3px #3498db;
+  background-color: #3498db;
+}
 </style>
