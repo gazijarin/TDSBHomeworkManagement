3e8ef447539bd3dfffa34cea337fcf6cc433210b
Gazi Jarin <gazijarin@gmail.com>
2019-11-28 13:34:52 -0500

    Modified progress page

--- front-end/src/components/progress/progress.vue
+++ front-end/src/components/progress/progress.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
+    <navbar></navbar>
     <h1>Progress</h1>
     <p>This is the progress page</p>
   </div>
 </template>
 
 <script>
+import navbar from '../navbar/navbar';
+
 export default {
-  name: 'Progress' //this is the name of the component
+  name: 'Progress', //this is the name of the component
+  components: {
+    'navbar': navbar
+  }
 }
 </script>
 <style>
