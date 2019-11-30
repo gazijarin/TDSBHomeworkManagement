880101925b8d95a91cb7e08e9a9ee083380ebc1c
Raiyan Rahman <ray.rahmn@hotmail.com>
2019-11-30 03:42:35 -0500

    Updated progress page.

--- front-end/src/components/progress/progress.vue
+++ front-end/src/components/progress/progress.vue
@@ -4,71 +4,104 @@
     <div class="col-4" style="float: right; padding-top: 15px;">
       <div class="card" style="border:1px solid black; margin-bottom:20px">
         <div class="card-header">
-          <h6 style="float: left; padding-top: 10px">Assignments</h6>
-          <b-button v-b-modal.modal-1 size="sm" style="float: right; margin-left: 15px;">
-            Add Assignment
-            <font-awesome-icon :icon="['fas', 'plus']" />
-          </b-button>
-          <b-modal
-            id="modal-1"
-            size="lg"
-            centered
-            ref="modal"
-            title="Add Assignment"
-            @show="resetModal"
-            @hidden="resetModal"
-            @ok="handleOk"
-          >
-            <div class="form-group">
-              <b-form-input v-model="text" placeholder="Title"></b-form-input>
-            </div>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <datepicker placeholder="Due Date"></datepicker>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <input class="form-control" type="time" value="12:00:00" style="margin-top: 2%" />
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
-              <select class="form-control">
-                <option>Course</option>
-              </select>
-            </div>
-            <!-- <div class="form-group">
-              <input type="number" class="form-control" id="GradeInput" placeholder="Grade" />
-            </div>-->
-            <label for="completedAssignment">Amount Completed</label>
-            <div class="form-group">
-              <vue-slider v-model="value" :marks="marks">
-                <template v-slot:step="{ label, active }">
-                  <div :class="['custom-step', { active }]"></div>
-                </template>
-              </vue-slider>
-            </div>
-            <label for="attachments">Attachment(s)</label>
-            <div class="form-group">
-              <input type="file" class="form-control-file" id="exampleFormControlFile1" />
-            </div>
-          </b-modal>
+          <h6 style="float: left; padding-top: 10px">Courses</h6>
           <div class="form-group">
-            <select class="form-control">
+            <select class="form-control" v-model="selectedCourse">
+              <option value="" selected disabled hidden>Pick a Course</option>
               <option v-for="course in courseList" :key="course">{{course}}</option>
             </select>
           </div>
-        </div>
-      </div>
-      <div class="card" style="border:1px solid black;">
-        <div class="card-header">
-          <h6 style="float: left; padding-top: 10px">Grades</h6>
-          <div class="form-group">
-            <select class="form-control">
-              <option v-for="course in courseList" :key="course">{{course}}</option>
-            </select>
+          <div v-if="selectedCourse">
+            <b-modal
+              id="modal-1"
+              size="lg"
+              centered
+              ref="modal"
+              title="Add Assignment"
+              @show="resetModal"
+              @hidden="resetModal"
+              @ok="handleOk"
+            >
+              <div class="form-group">
+                <b-form-input v-model="text" placeholder="Title"></b-form-input>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <datepicker placeholder="Due Date"></datepicker>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input class="form-control" type="time" value="12:00:00" style="margin-top: 2%" />
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <select class="form-control">
+                  <option>Course</option>
+                </select>
+              </div>
+              <!-- <div class="form-group">
+                <input type="number" class="form-control" id="GradeInput" placeholder="Grade" />
+              </div>-->
+              <label for="completedAssignment">Amount Completed</label>
+              <div class="form-group">
+                <vue-slider v-model="value" :marks="marks">
+                  <template v-slot:step="{ label, active }">
+                    <div :class="['custom-step', { active }]"></div>
+                  </template>
+                </vue-slider>
+              </div>
+              <label for="attachments">Attachment(s)</label>
+              <div class="form-group">
+                <input type="file" class="form-control-file" id="exampleFormControlFile1" />
+              </div>
+            </b-modal>
+            <hr>
+            <b-tabs content-class="mt-3" fill pills>
+              <b-tab title="Assignments" active>
+                <b-card>
+                  <template v-slot:header>
+                    <h5 class="mb-0" style="float: left; margin-left: 15px; margin-top: 5px">Names</h5>
+                    <b-button v-b-modal.modal-1 size="sm" style="float: right; margin-left: 15px;">
+                      Add Assignment
+                      <font-awesome-icon :icon="['fas', 'plus']" />
+                    </b-button>
+                  </template>
+                  <b-list-group flush>
+                    <b-list-group-item v-for="assignment in assignmentList" :key="assignment">
+                      <p style="float: left">{{assignment.name}}</p>
+                      <b-button v-b-modal.modal-1 size="sm" variant="danger" style="float: right; margin-top: -5px; margin-left: 10px;">
+                        Delete
+                        <font-awesome-icon :icon="['fas', 'trash']" />
+                      </b-button>
+                      <b-button v-b-modal.modal-1 variant="info" size="sm" style="float: right; margin-top: -5px;">
+                        Edit
+                        <font-awesome-icon :icon="['fas', 'edit']" />
+                      </b-button>
+                    </b-list-group-item>
+                  </b-list-group>
+                </b-card>
+              </b-tab>
+              <b-tab title="Grades">
+                <b-card>
+                  <template v-slot:header>
+                    <h5 class="mb-0" style="float: left; margin-left: 15px; margin-top: 5px">Names</h5>
+                    <b-button v-b-modal.modal-1 size="sm" style="float: right; margin-left: 15px;">
+                      Add Assignment
+                      <font-awesome-icon :icon="['fas', 'plus']" />
+                    </b-button>
+                  </template>
+                  <b-list-group flush>
+                    <b-list-group-item v-for="assignment in assignmentList" :key="assignment">
+                      <p style="float: left">{{assignment.name}}</p>
+                      <b-form-input :state="nameState(assignment.grade)" id="input-grade" size="sm" v-model="assignment.grade" style="float: right; width: 30%" placeholder="Grade"></b-form-input>
+                    </b-list-group-item>
+                  </b-list-group>
+                </b-card>
+              </b-tab>
+            </b-tabs>
           </div>
         </div>
       </div>
@@ -85,7 +118,12 @@
               {
                 data: chartData,
                 smooth: true,
-                fill: true
+                fill: false
+              },
+              {
+                data: [10, 30, 75, 90, 100, 15, 65, 0, 80, 60, 90, 55, 95, 35, 50],
+                smooth: true,
+                fill: false
               }
             ]"
             :grid="{
@@ -144,8 +182,8 @@ export default {
   data() {
     return {
       value: 50,
-      allAssignments: true,
-      allGrades: true,
+      selectedCourse: "",
+      assignmentList: [{name: "A1", grade: 88}, {name: "A2", grade: 79}, {name: "A3", grade: 98}],
       chartData: [100, 20, 55, 90, 50, 10, 35, 55, 60, 80, 100, 85, 25, 95, 70],
       xAxisList: ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15"],
       barGraphData: [{label: 'A1', value: 40}, {label: 'A2', value: 10}, {label: 'A3', value: 100}, {label: 'A4', value: 60}],
@@ -170,6 +208,17 @@ export default {
     getCompletionPercentage: function(assignmentList) {
       // Convert the assignments to their completion percentage.
       return assignmentList
+    },
+    nameState: function(grade) {
+      return grade >= 0 && grade <= 100 ? true : false
+    }
+  },
+  computed: {
+    allAssignments: function() {
+      return this.selectedCourse === "All"
+    },
+    allGrades: function() {
+      return this.selectedCourse === "All"
     }
   }
 };
