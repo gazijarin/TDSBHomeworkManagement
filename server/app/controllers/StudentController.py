2de624c8edd7f526567c9ee0a6869fabca27a3ca
Anusha Fazal <fazalanu@teach.cs.toronto.edu>
2019-12-01 06:25:47 -0500

    Add sync time to POST

--- server/app/controllers/StudentController.py
+++ server/app/controllers/StudentController.py
@@ -1,11 +1,14 @@
 from app.models.Student import Student
 import uuid
+import datetime
 
 student = Student()
 
 def post(first_name, last_name, image, email, _id, courses):
-    student.insert({"first_name": first_name, 'last_name': last_name,'_id': _id, "image": image,
-                    "email": email, "courses": courses})
+    payload = {"first_name": first_name, 'last_name': last_name,'_id': _id, "image": image,
+                    "email": email, "courses": courses, "created": datetime.datetime.utcnow()}
+    payload["last_sync_time"] = payload["created"]
+    student.insert(payload)
 
 def get(_id):
     return student.get(_id)
