315818a4ade2cf4d7f563165a10a380f7c3c044e
SirAnirudh <anirudhharibaskar@gmail.com>
2019-11-30 23:38:04 -0500

    - Reorganized Routes, student structures, connected to classroom aapi

--- server/app/controllers/StudentController.py
+++ server/app/controllers/StudentController.py
@@ -2,6 +2,11 @@ from app.models.Student import Student
 import uuid
 
 
-def post(first_name, last_name, image, email, _id):
-    new_student = Student(first_name= first_name, last_name=last_name, _id=_id, image=image, email=email)
-    new_student.insert()
+def post(first_name, last_name, image, email, _id, courses):
+    student = Student()
+    student.insert({"first_name": first_name, 'last_name': last_name,'_id': _id, "image": image,
+                    "email": email, "courses": courses})
+
+def get(id):
+    student = Student(id)
+    return student.get(id)
