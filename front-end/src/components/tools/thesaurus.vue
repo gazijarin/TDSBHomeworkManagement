f8f627659aa40eda62fb39999f12a6677d3b773b
Gazi Jarin <gazijarin@gmail.com>
2019-12-01 14:40:31 -0500

    Added thesaurus

---
+++ front-end/src/components/tools/thesaurus.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row" id="Thesaurus">
+    <form class="form-inline">
+      <div class="form-group mx-sm-3 mb-2">
+        <b-form-input v-model="wordText" placeholder="Enter a word."></b-form-input>
+      </div>
+      <button type="submit" class="btn btn-primary mb-2">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Thesaurus",
+  data() {
+    return {
+      wordText: ""
+    };
+  },
+  methods: {
+    formSubmit(e) {
+      alert(this.wordText);
+      this.$emit("formSubmit", this.wordText);
+      e.preventDefault();
+    }
+  }
+};
+</script>
+
+<style>
+/* #transForm {
+  border-radius: 10px;
+  border: 1px #ccc solid;
+} */
+</style>
